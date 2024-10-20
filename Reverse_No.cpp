08734ed437d8750547045815d1a20c6ddd5df8cc
msurbhit <maniyarsurbhit@gmail.com>
2024-10-21 00:22:11 +0530

    Create Reverse_No.cpp

---
+++ Reverse_No.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+int rev(int n) 
+{ 
+	int new_n = 0; 
+	while (n > 0) { 
+		new_n = new_n * 10 + n % 10; 
+		n = n % 10; 
+	} 
+	return new_n; 
+} 
+
+int main() 
+{ 
+	int n = 4562; 
+	cout << "Reverse of num is " << rev(n); 
+	return 0; 
+}
